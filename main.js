6c389ed13bd35948d3f1ce6cc2a9915a50ef71db
turik777 <52492191+turik777@users.noreply.github.com>
2024-05-10 22:43:55 +0300

    Add functions for game results and buttons

--- main.js
+++ main.js
@@ -7,12 +7,15 @@ const ticTacToe = (function () {
     
     const players = {
         playerOne: "X",
-        playerTwo: "O"
+        playerTwo: "O",
+        playerOneName: "Player 1",
+        playerTwoName: "Player 2"
     };
 
     const gameFlow = {
         playerTurn: "",
         gameResult: "",
+        isGameStarted: false,
 
         checkWinner: function () {
             if ((gameboard.tiles[0] === "X" && gameboard.tiles[1] === "X" && gameboard.tiles[2] === "X") ||
@@ -23,7 +26,7 @@ const ticTacToe = (function () {
                 (gameboard.tiles[2] === "X" && gameboard.tiles[5] === "X" && gameboard.tiles[8] === "X") ||
                 (gameboard.tiles[0] === "X" && gameboard.tiles[4] === "X" && gameboard.tiles[8] === "X") ||
                 (gameboard.tiles[2] === "X" && gameboard.tiles[4] === "X" && gameboard.tiles[6] === "X")) {
-                gameFlow.gameResult = "Player One WINS!"
+                gameFlow.gameResult = players.playerOneName + " WINS!";
             } else if ((gameboard.tiles[0] === "O" && gameboard.tiles[1] === "O" && gameboard.tiles[2] === "O") ||
                        (gameboard.tiles[3] === "O" && gameboard.tiles[4] === "O" && gameboard.tiles[5] === "O") ||
                        (gameboard.tiles[6] === "O" && gameboard.tiles[7] === "O" && gameboard.tiles[8] === "O") ||
@@ -32,13 +35,17 @@ const ticTacToe = (function () {
                        (gameboard.tiles[2] === "O" && gameboard.tiles[5] === "O" && gameboard.tiles[8] === "O") ||
                        (gameboard.tiles[0] === "O" && gameboard.tiles[4] === "O" && gameboard.tiles[8] === "O") ||
                        (gameboard.tiles[2] === "O" && gameboard.tiles[4] === "O" && gameboard.tiles[6] === "O")) {
-                gameFlow.gameResult = "Player Two WINS!"
+                gameFlow.gameResult = players.playerTwoName + " WINS!";
             } else {
                 if (gameboard.tiles.join("").length === 9) gameFlow.gameResult = "TIE!";
             }
+            displayDOM.updateInfo();
         },
 
-        displayResult: () => {console.log(gameFlow.gameResult);},
+        startGame: function () {
+            gameFlow.isGameStarted = true;
+            displayDOM.updateInfo();
+        },
 
         resetGame: () => {
             gameboard.tiles = ["", "", "",
@@ -46,10 +53,17 @@ const ticTacToe = (function () {
                                "", "", ""];
             gameFlow.playerTurn = "";
             gameFlow.gameResult = "";
+
+            const gameboardTiles = Array.from(document.querySelectorAll(".gameboard-tile"));
+            gameboardTiles.forEach(gameboardTile => {
+                gameboardTile.textContent = "";
+            });
+            gameFlow.isGameStarted = false;
+            displayDOM.updateInfo();
         },
 
         addMark: function(tile) {
-            if (gameboard.tiles[tile] || gameFlow.gameResult) return;
+            if (gameboard.tiles[tile] || gameFlow.gameResult || !this.isGameStarted) return;
     
             if (gameFlow.playerTurn === "X") {
                 gameboard.tiles[tile] = players["playerTwo"];
@@ -58,10 +72,9 @@ const ticTacToe = (function () {
                 gameboard.tiles[tile] = players["playerOne"];
                 gameFlow.playerTurn = gameboard.tiles[tile];
             }
+            displayDOM.updateInfo();
     
             gameFlow.checkWinner();
-            if (gameFlow.gameResult) gameFlow.displayResult();
-            console.log(gameboard.tiles);
         }
     };
 
@@ -79,7 +92,7 @@ const ticTacToe = (function () {
             });
         })(),
 
-        displayMark: (function (gameboardTile) {
+        displayMark: (function () {
             const gameboardTiles = Array.from(document.querySelectorAll(".gameboard-tile"));
 
             gameboardTiles.forEach(gameboardTile => {
@@ -88,9 +101,42 @@ const ticTacToe = (function () {
                     gameboardTile.textContent = gameboard.tiles[gameboardTiles.indexOf(gameboardTile)];
                 })
             });
-        })()
+        })(),
+
+        displayInfo: (function () {
+            const gameInfo = document.createElement("p");
+            gameInfo.classList.add("game-info");
+            document.body.appendChild(gameInfo);
+        })(),
+
+        updateInfo: function () {
+            players.playerOneName = document.querySelector("#player-one").value || players.playerOneName;
+            players.playerTwoName = document.querySelector("#player-two").value || players.playerTwoName;
+            const gameInfo = document.querySelector(".game-info");
+
+            if (gameFlow.playerTurn === "O" || !gameFlow.playerTurn) {
+                gameInfo.textContent = players.playerOneName + "'s Turn";
+            } else if (gameFlow.playerTurn === "X") {
+                gameInfo.textContent = players.playerTwoName + "'s Turn";
+            }
+            if (gameFlow.isGameStarted === false) gameInfo.textContent = "";
+            if (gameFlow.gameResult) gameInfo.textContent = gameFlow.gameResult;
+        },
     };
 
-    console.log(gameboard.tiles);
-    return {gameFlow, displayDOM};
+    const buttons = {
+        startButton: (() => {
+            const start = document.querySelector(".start");
+            start.addEventListener("click", () => {
+                gameFlow.startGame();
+            })
+        })(),
+
+        resetButton: (() => {
+            const reset = document.querySelector(".reset");
+            reset.addEventListener("click", () => {
+                gameFlow.resetGame();
+            })
+        })()
+    };
 })();
